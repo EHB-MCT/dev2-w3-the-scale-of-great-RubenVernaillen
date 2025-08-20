8cae42791222fa5d05e761841a8db1412e7cebc1
Ruben Vernaillen <Ruben.vernaillen@student.ehb.be>
2025-08-20 13:10:51 +0200

    finished

--- js/main.js
+++ js/main.js
@@ -6,32 +6,120 @@ let adjectives;
 let sortDirection = "up";
 
 function init() {
+    console.log("Let's start");
 
+    adjectives = JSON.parse(getAdjectives());
+    console.log(adjectives);
+    render();
+    addSortEvents();
 }
 
 function addSortEvents() {
+    document.querySelector('#sort-up').addEventListener('click', function (event) {
+        this.classList.add('active');
+        document.querySelector('#sort-down').classList.remove('active');
+        console.log("Sort up!");
+        sortDirection = "up";
+        sort();
+    });
+
+    document.querySelector('#sort-down').addEventListener('click', function () {
+        this.classList.add('active');
+        document.querySelector('#sort-up').classList.remove('active');
+        console.log("Sort down!");
+        sortDirection = "down";
+        sort();
+    });
 
 }
 
 function addVoteEvents() {
+    const upvoteButtons = document.querySelectorAll('.upvote-button');
+    upvoteButtons.forEach(function (button) {
+        button.addEventListener('click', function (event) {
+            console.log(event.target);
+            upVote(event.target);
+        })
+    });
 
+    const downvoteButtons = document.querySelectorAll('.downvote-button');
+    downvoteButtons.forEach(function (button) {
+        button.addEventListener('click', function (event) {
+            console.log(event.target);
+            downVote(event.target);
+        })
+    })
 }
 
 function sort() {
+    console.log("This is the sorting function");
 
+    if (sortDirection == 'down') {
+        adjectives.sort(function (a, b) {
+            if (a.score > b.score) {
+                return -1
+            } else {
+                return 1
+            }
+        });
+    } else {
+        adjectives.sort(function (a, b) {
+            if (a.score > b.score) {
+                return 1
+            } else {
+                return -1
+            }
+        });
+    }
+
+    render();
 }
 
 function render() {
+    console.log("render!");
 
+    //create htmlstring
+    let htmlString = '';
+
+    adjectives.forEach(function (adjective) {
+        let scoreClass = 'bad'
+        if (adjective.score >= 7) {
+            scoreClass = 'good'
+        }
+        else if (adjective.score >= 5) {
+            scoreClass = 'average'
+        }
+
+
+        htmlString += `
+        <div class="word-item">
+            <span class="word-score ${scoreClass}">${adjective.score}</span>
+            <span>${adjective.word}</span>
+            <div class="vote-buttons">
+                <button value="${adjective.word}" class="upvote-button">👍</button>
+                <button value="${adjective.word}" class="downvote-button">👎</button>
+            </div>
+        </div>
+        `;
+    })
+
+    //add htmlString to HTML
+    document.querySelector('#container').innerHTML = htmlString;
+
+    addVoteEvents();
 }
 
 function upVote(target) {
-
+    console.log("Upvote", target.value);
+    updateScore(target.value, 0.1);
+    render();
 }
 
 
 function downVote(target) {
-
+    console.log("Downvote", target.value);
+    updateScore(target.value, -0.1);
+    render();
 }
 
 function updateScore(word, scoreChange) {
